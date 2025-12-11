7a8039fe218ee7f5a9b1dd4d398ad9f97a9d14ca
DaniglezW <dani.flex.work@gmail.com>
2025-12-11 14:57:37 +0000

    Fix :fix:: Fixed stars bug

--- app/app/components/StarRating.tsx
+++ app/app/components/StarRating.tsx
@@ -2,14 +2,15 @@ import { StarIcon as SolidStarIcon } from '@heroicons/react/24/solid';
 import { StarIcon as OutlineStarIcon } from '@heroicons/react/24/outline';
 
 type StarRatingProps = {
-    rating: number; // 1 a 5
+    rating?: number; // 1 a 5
 };
 
 export default function StarRating({ rating }: Readonly<StarRatingProps>) {
+    const safeRating = rating ?? 0;
     return (
         <div className="flex space-x-1">
             {[1, 2, 3, 4, 5].map((num) =>
-                num <= rating ? (
+                num <= safeRating ? (
                     <SolidStarIcon key={num} className="w-6 h-6 text-yellow-400" />
                 ) : (
                     <OutlineStarIcon key={num} className="w-6 h-6 text-gray-300" />
